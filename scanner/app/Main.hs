4d0e137f7ad08e05568dabd69264f3be34be4764
VladimirLogachev <vladimir@logachev.dev>
2024-05-16 20:46:04 +0400

    Install relude

--- scanner/app/Main.hs
+++ scanner/app/Main.hs
@@ -1,6 +1,7 @@
 module Main (main) where
 
 import Lib
+import Relude
 
 main :: IO ()
 main = someFunc
--- scanner/src/Lib.hs
+++ scanner/src/Lib.hs
@@ -1,6 +1,6 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (someFunc) where
+
+import Relude
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
